a4451bac964e2d17c1b8bf9ec9186ffe851339b0
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2025-08-15 07:33:48 -0300

    feat: Adicionando data e novas rotas
    Adicionando mais times e corredores, alem de adicionar as rotas post, put e delete para times e corredores.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
 
+const PORT = parseInt(process.env.PORT || "3333");
+
 const server = fastify({ logger: true });
 
 server.register(cors, {
@@ -8,24 +10,39 @@ server.register(cors, {
 });
 
 const teams = [
-  { id: 1, name: "McLaren", base: "Woking, United Kingdom" },
-  { id: 2, name: "Mercedes", base: "Brackley, United Kingdom" },
-  { id: 3, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
-  { id: 4, name: "Ferrari", base: "Maranello, Italy" },
-  { id: 5, name: "Alpine", base: "Enstone, United Kingdom" },
-  { id: 6, name: "Aston Martin", base: "Silverstone, United Kingdom" },
-  { id: 7, name: "Alfa Romeo Racing", base: "Hinwil, Switzerland" },
-  { id: 8, name: "AlphaTauri", base: "Faenza, Italy" },
+  { id: 1, name: "Red Bull Racing", base: "Milton Keynes, United Kingdom" },
+  { id: 2, name: "Ferrari", base: "Maranello, Italy" },
+  { id: 3, name: "McLaren", base: "Woking, United Kingdom" },
+  { id: 4, name: "Mercedes", base: "Brackley, United Kingdom" },
+  { id: 5, name: "Aston Martin", base: "Silverstone, United Kingdom" },
+  { id: 6, name: "Alpine", base: "Enstone, United Kingdom" },
+  { id: 7, name: "Haas", base: "Kannapolis, United States" },
+  { id: 8, name: "Racing Bulls", base: "Faenza, Italy" },
   { id: 9, name: "Williams", base: "Grove, United Kingdom" },
-  { id: 10, name: "Haas", base: "Kannapolis, United States" },
-  { id: 11, name: "Uralkali Haas F1 Team", base: "Banbury, United Kingdom" },
-  { id: 12, name: "Scuderia Toro Rosso", base: "Faenza, Italy" },
+  { id: 10, name: "Kick Sauber", base: "Hinwil, Switzerland" },
 ];
 
 const drivers = [
   { id: 1, name: "Max Verstappen", team: "Red Bull Racing" },
-  { id: 2, name: "Lewis Hamilton", team: "Ferrari" },
-  { id: 2, name: "Lando Norris", team: "McLaren" },
+  { id: 2, name: "Liam Lawson", team: "Red Bull Racing" },
+  { id: 3, name: "Charles Leclerc", team: "Ferrari" },
+  { id: 4, name: "Lewis Hamilton", team: "Ferrari" },
+  { id: 5, name: "Lando Norris", team: "McLaren" },
+  { id: 6, name: "Oscar Piastri", team: "McLaren" },
+  { id: 7, name: "George Russell", team: "Mercedes" },
+  { id: 8, name: "Kimi Antonelli", team: "Mercedes" },
+  { id: 9, name: "Fernando Alonso", team: "Aston Martin" },
+  { id: 10, name: "Lance Stroll", team: "Aston Martin" },
+  { id: 11, name: "Pierre Gasly", team: "Alpine" },
+  { id: 12, name: "Jack Doohan", team: "Alpine" },
+  { id: 13, name: "Esteban Ocon", team: "Haas" },
+  { id: 14, name: "Oliver Bearman", team: "Haas" },
+  { id: 15, name: "Yuki Tsunoda", team: "Racing Bulls" },
+  { id: 16, name: "Isack Hadjar", team: "Racing Bulls" },
+  { id: 17, name: "Alexander Albon", team: "Williams" },
+  { id: 18, name: "Carlos Sainz", team: "Williams" },
+  { id: 19, name: "Nico Hülkenberg", team: "Kick Sauber" },
+  { id: 20, name: "Gabriel Bortoleto", team: "Kick Sauber" },
 ];
 
 server.get("/teams", async (request, response) => {
@@ -38,11 +55,27 @@ server.get("/drivers", async (request, response) => {
   return { drivers };
 });
 
-interface DriverParams {
+interface Params {
   id: string;
 }
 
-server.get<{ Params: DriverParams }>(
+server.get<{ Params: Params}>(
+  "/teams/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+    const team = teams.find((t) => t.id === id);
+
+    if(!team) {
+      response.type("application/json").code(404);
+      return { message: "Team Not Found" };
+    } else {
+      response.type("application/json").code(200);
+      return { team };
+    }
+  }
+);
+
+server.get<{ Params: Params }>(
   "/drivers/:id",
   async (request, response) => {
     const id = parseInt(request.params.id);
@@ -58,6 +91,136 @@ server.get<{ Params: DriverParams }>(
   }
 );
 
-server.listen({ port: 3333 }, () => {
-  console.log("Server init");
+interface Team {
+  name: string;
+  base: string;
+}
+
+interface Driver {
+  name: string;
+  team: string;
+}
+
+server.post<{ Body: Team }>(
+  "/teams/",
+  async (request, response) => {
+    const body = request.body;
+    
+    if(!body.name || !body.base) {
+      response.type("application/json").code(400);
+      return { message: "Missing parameters" };
+    } else {
+      teams.push({
+        id: teams.length + 1,
+        ...body,
+      });
+
+      response.type("application/json").code(201);
+      return { message: "Created" };
+    }
+  }
+);
+
+server.post<{ Body: Driver }>(
+  "/drivers/",
+  async (request, response) => {
+    const body = request.body;
+
+    if(!body.name || !body.team) {
+      response.type("application/json").code(400);
+      return { message: "Missing parameters" };
+    } else {
+      drivers.push({
+        id: drivers.length + 1,
+        ...body,
+      });
+
+      response.type("application/json").code(201);
+      return { message: "Created" };
+    }
+  }
+);
+
+server.put<{Params: Params, Body: Team}>(
+  "/team/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+    const body = request.body;
+
+    const teamIndex = teams.findIndex(team => team.id === id);
+
+    if(teamIndex === -1) {
+      response.type("application/json").code(404);
+      return { message: "Team Not Found" };
+    } else {
+      const newBody = { id: id, ...body };      
+      teams[teamIndex] = newBody;
+
+      response.type("application/json").code(200);
+      return { message: "OK"};
+    }
+  }
+);
+
+server.put<{Params: Params, Body: Driver}>(
+  "/drivers/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+    const body = request.body;
+
+    const driverIndex = drivers.findIndex(driver => driver.id === id);
+
+    if(driverIndex === -1) {
+      response.type("application/json").code(404);
+      return { message: "Driver Not Found" };
+    } else {
+      const newBody = { id: id, ...body};
+      drivers[driverIndex] = newBody;
+
+      response.type("application/json").code(200);
+      return { message: "OK" };
+    }
+  }
+);
+
+server.delete<{Params: Params}>(
+  "/teams/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+
+    const teamIndex = teams.findIndex(team => team.id === id);
+
+    if(teamIndex === -1) {
+      response.type("application/json").code(404);
+      return { message: "Team Not Found" };
+    } else {
+      drivers.splice(teamIndex, 1);
+
+      response.type("application/json").code(200);
+      return { message: "OK" };
+    }
+  }
+);
+
+server.delete<{Params: Params}>(
+  "/drivers/:id",
+  async (request, response) => {
+    const id = parseInt(request.params.id);
+
+    const driverIndex = drivers.findIndex(driver => driver.id === id);
+
+    if(driverIndex === -1) {
+      response.type("application/json").code(404);
+      return { message: "Team Not Found" };
+    } else {
+      drivers.splice(driverIndex, 1);
+
+      response.type("application/json").code(200);
+      return { message: "OK" };
+    }
+  }
+);
+
+server.listen({ port: PORT }, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
